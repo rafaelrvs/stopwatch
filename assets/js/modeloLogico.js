8d7046955d801ac373fb4c99a83ed97bf4729136
rafaelrvs <rafaelvieira1333@gmail.com>
2023-03-27 12:48:58 -0300

    Commit inicial

---
+++ assets/js/modeloLogico.js
@@ -0,0 +1,48 @@
+var sec = 0;
+var min = 0;
+var hr = 0;
+var intervalo = 0;
+var digito = 0;
+
+function doisdigitos(digito){
+ if(digito<10){
+    return '0'+ digito;
+ }
+ else{
+    return digito;
+ }
+
+}
+function play(){
+    watch();
+    intervalo= setInterval(watch,0.5);
+
+}
+function stopAction(){
+    clearInterval(intervalo);
+
+}
+  
+function resetAction(){
+    clearInterval(intervalo);
+    hr=0;
+    min =0;
+    sec=0;
+    document.getElementById('watch').innerHTML='00:00:00';
+}
+
+
+function watch(){
+    sec++
+    if(sec == 60){
+        min++;
+        sec=0;
+        
+    }
+    if(min == 60){
+        hr++;
+        min=0;
+    }
+    document.getElementById('watch').innerHTML=doisdigitos(hr)  + ':' +doisdigitos(min) + ':' + doisdigitos(sec);
+
+}
